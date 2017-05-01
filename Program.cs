c0df2f14f9465768b7d9b4467548835ebf6a8fd7
Eric Zhao <zhao.yichao15@gmail.com>
2017-05-01 20:15:24 +1000

    intialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ice.CSharp.Foundation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
